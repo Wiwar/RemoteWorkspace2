d885fe0953aa9862db4b4e604645cd9010c8e901
Wiwar <michi@DESKTOP-NGJJ9O9.lan>
2023-08-01 16:49:12 -0400

    see you

---
+++ src/main/java/net/mcreator/test/procedures/MagictaxProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.test.procedures;
+
+import java.util.Map;
+
+public class MagictaxProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
